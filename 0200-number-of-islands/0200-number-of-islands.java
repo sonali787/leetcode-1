86dc1417ffcde60656ef35231dd44563ecdaa6be
sonali787 <sonaliy4535@gmail.com>
2026-09-21 11:40:10 +0530

    Time: 7 ms (14.59%), Space: 52.6 MB (13.58%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,19 +1,21 @@
-class pair{
+class Node{
     int i;
     int j;
-    pair(int i,int j){
-        this.i =i;
-        this.j = j;
+
+    Node(int i,int j){
+        this.i=i;
+        this.j=j;
     }
 }
+
 class Solution {
-    
+
     void dfs(char[][] grid,int i,int j,int n,int m,boolean[][] vis){
         if(i<0 || i>=n || j<0 || j>=m){
-          return;
+            return;
         }
 
-        if(grid[i][j]=='0' || vis[i][j]==true){
+         if(grid[i][j]=='0' || vis[i][j]==true){
            return;
         }
         vis[i][j]=true;
@@ -24,45 +26,32 @@ class Solution {
         dfs(grid,i-1,j,n,m,vis);
 
     }
-
     public int numIslands(char[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
+        boolean[][] vis=new boolean[n][m];
 
-        Queue<pair> q = new LinkedList<>();
-
+        Queue<Node> q= new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1'){
-                    q.add(new pair(i,j));
+                    q.add(new Node(i,j));
                 }
             }
         }
-
-        boolean[][] vis=new boolean[n][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                vis[i][j]=false;
-            }
-        }
-
         int count=0;
+
         while(!q.isEmpty()){
-            pair ele = q.poll();
-            int i=ele.i;
-            int j=ele.j;
+            Node curr_node = q.poll();
+            int i=curr_node.i;
+            int j=curr_node.j;
 
             if(vis[i][j]==false){
-                count = count+1;
                 dfs(grid,i,j,n,m,vis);
+                                count++;
             }
-
         }
 
-
         return count;
-
-
-
     }
 }
